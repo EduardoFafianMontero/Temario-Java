490d0376504ab4559fb5e98254df46d50e6817ae
EduardoFafianMontero <e.fafianmontero@gmail.com>
2026-09-16 12:58:27 +0200

    Empezar aprender las vaiables

---
+++ Variables/Ejercicios/Variables.java
@@ -0,0 +1,13 @@
+public class Variables{
+  public static void main (String[] args){
+    //Variables//
+    int edad=31;//tipo de dato entero//
+    double precio=300.4;//tipo de dato decimal//
+    boolean disponible= true; // True /False//
+    char genero='M'; //M-Masculino / F-Femenino//
+    //Accede a las variables//
+    System.out.println(edad);
+    // Modifica a las vaiables//
+    edad = 35;
+  }
+}
