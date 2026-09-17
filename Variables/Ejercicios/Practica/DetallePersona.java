a665f5e42bb94cf18ad7b28a228aeb9b846c4434
EduardoFafianMontero <e.fafianmontero@gmail.com>
2026-09-17 11:57:16 +0200

    Practica para aprender sobre los detalles

---
+++ Variables/Ejercicios/Practica/DetallePersona.java
@@ -0,0 +1,16 @@
+public class DetallePersona{
+  public static void main(String[] args){
+    //Detalle de una Persona//
+    String nombre="Eduardo";
+    int edad=21;
+    double estatura=1.70;
+    String PaisOrigen="España";
+    char casado='S';
+    //Impimir el detalle de la persona//
+    System.out.println(nombre);
+    System.out.println(edad);
+    System.out.println(estatura);
+    System.out.println(PaisOrigen);
+    System.out.println(casado);
+  }
+}
