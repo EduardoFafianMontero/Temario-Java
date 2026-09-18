aecc7cbc6ea36053e901358b1abb83c0273676af
EduardoFafianMontero <e.fafianmontero@gmail.com>
2026-09-18 10:57:03 +0200

    Correguimos y añadimos un poco mas al ejercicio

--- Variables/Ejercicios/Practica/DetalleProductoTienda/DetalleProductoTienda.java
+++ Variables/Ejercicios/Practica/DetalleProductoTienda/DetalleProductoTienda.java
@@ -7,9 +7,21 @@ public class DetalleProductoTienda{
      boolean Disponible=true;
 
      //Imprimir el producto//
-     System.out.println(NombreProducto);
-     System.out.println(PrecioProducto);
-     System.out.println(CantidadProducto);
-     System.out.println(Disponible);
+     System.out.println("NombreProducto"+NombereProducto);
+     System.out.println("PrecioProducto"+PrecioProducto);
+     System.out.println("CantidadProducto"+CantidadProducto);
+     System.out.println("Disponible"+ Disponible);
+
+     //Modificar el producto//
+     NombreProducto="Camara canon";
+     PrecioPodcuto=500.90;
+     CantidadProdcuto=0;
+     Disponible=false;
+
+     //Imprimir el producto//
+     System.out.println("NombreProducto"+NombereProducto);
+     System.out.println("PrecioProducto"+PrecioProducto);
+     System.out.println("CantidadProducto"+CantidadProducto);
+     System.out.println("Disponible"+ Disponible);
     }
 }
