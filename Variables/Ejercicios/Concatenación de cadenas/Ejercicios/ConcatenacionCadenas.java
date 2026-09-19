1dd752d892441a1383595fd69a0e59075196e089
EduardoFafianMontero <e.fafianmontero@gmail.com>
2026-09-19 10:07:28 +0200

    Aprendiendo a concatenar en Java

---
+++ Variables/Ejercicios/Concatenación de cadenas/Ejercicios/ConcatenacionCadenas.java
@@ -0,0 +1,9 @@
+public class ConcatenacionCadenas{
+  public static void main(String[] ags){
+    System.out.println("*** Concatenació de Cadenas *** ");
+    var nombre="Edu";
+    var apellido="Fafián";
+    var nombrecompleto= nombre + apellido:
+    System.out.printlm("Nombre completo="+nombrecompleto);
+  }
+}
