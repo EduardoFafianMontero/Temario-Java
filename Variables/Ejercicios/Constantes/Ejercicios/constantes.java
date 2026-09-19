bec8a9df299fca08e0a2ad44e57b43dc217746d1
EduardoFafianMontero <e.fafianmontero@gmail.com>
2026-09-19 10:25:58 +0200

    Apendiendo las cinstantes en Java

---
+++ Variables/Ejercicios/Constantes/Ejercicios/constantes.java
@@ -0,0 +1,13 @@
+public class constantes{
+  public static void main(String[] args){
+    System.out.println("*** Uso de Constantes en Java ***");
+    final var Dias_Semana=7;
+    System.out.println("Dias_semana="+Dias_Semana);
+    final var PI=3.1416;
+    System.out.println("Pi="+PI);
+    final var Mensaje_Biemvenida="Bienbenido a mi repositorio";
+    System.out.println("Mensaje de bienbenida="+Mensaje_Biemvenida);
+    final var minutos_por_segundo=60;
+    System.out.println("minutos_por_segundo="+minutos_por_segundo);
+  }
+}
