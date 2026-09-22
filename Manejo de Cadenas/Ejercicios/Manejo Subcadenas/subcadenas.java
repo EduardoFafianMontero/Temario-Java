5475c8172d59986e413fd75bdbc46b0ad5f1ee64
EduardoFafianMontero <e.fafianmontero@gmail.com>
2026-09-22 11:45:57 +0200

    Aprendiendo subcadenas

---
+++ Manejo de Cadenas/Ejercicios/Manejo Subcadenas/subcadenas.java
@@ -0,0 +1,18 @@
+public class subcadenas{
+  public static void main(String[] args){
+    // Tema de Subcadenas //
+    var cadena1="Bienvenidos a mis recursos";
+    System.out.println("cadena1=" +cadena1);
+
+    // Subcadena //
+    var subcadena1= cadena1.substring(0, 11);
+    System.out.println("subcadena1= " + subcadena1);
+
+    // Practica pone la subcadena //
+     var cadena2="Bienvenidos a mis recursos";
+     System.out.println("cadena2=" +cadena2);
+      var subcadena2= cadena2.substring(20, 25);
+     System.out.println("subcadena2= " + subcadena2 );
+  }
+
+}
