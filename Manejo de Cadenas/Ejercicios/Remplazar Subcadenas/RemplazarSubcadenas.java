b1062703ce974977cd32983e777e9371d33a75fd
EduardoFafianMontero <e.fafianmontero@gmail.com>
2026-09-23 10:27:52 +0200

    Corregguion y ejecucion del achivo

--- Manejo de Cadenas/Ejercicios/Remplazar Subcadenas/RemplazarSubcadenas.java
+++ Manejo de Cadenas/Ejercicios/Remplazar Subcadenas/RemplazarSubcadenas.java
@@ -1,14 +1,15 @@
-public class RemplazarSubcadenas.java{
-  public static main(String[] args){
+public class RemplazarSubcadenas{
+  public static void main(String[] args){
     // Reemplazar subcadenas //
     var cadena="hola mundo";
     System.out.println("cadena = "+cadena);
 
     // Remplazar Munndo por a todos //
-    var nuevaCadena= cadena.eplace("mundo", "a todos");
-    System.out.println("nuevaCadena = "+cadena);
+    var nuevaCadena= cadena.replace("mundo", "a todos");
+    System.out.println("nuevaCadena = "+nuevaCadena);
 
     //Remplazar la subcadena Hola por Adios //
-    nuevaCadena = cadena.replace("Hola", "Saludos");
+    nuevaCadena = cadena.replace("hola", "Saludos");
+    System.out.println("nuevaCadena= "+nuevaCadena);
   }
 }
