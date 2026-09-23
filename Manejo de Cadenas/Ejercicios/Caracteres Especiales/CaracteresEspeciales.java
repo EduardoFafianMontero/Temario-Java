ad904d2bb97c01ddf32e0b00a1548c7bef0e3523
EduardoFafianMontero <e.fafianmontero@gmail.com>
2026-09-23 11:13:22 +0200

    Aprendiendo Carcteres especiales

---
+++ Manejo de Cadenas/Ejercicios/Caracteres Especiales/CaracteresEspeciales.java
@@ -0,0 +1,23 @@
+public class CaracteresEspeciales{
+  public static void mai(Sting[] args);
+  // Caracteres Especiales //
+  // \n - impimir un salto de linea //
+  var cadena1= "Hola  \n mundo";
+  system.out.println("cadena1 = "+ cadena1);
+
+  // '\t' = tabulador //
+  var cadena2= "\tHola \tmundo";
+  system.out.println("cadena2 = "+ cadena2);
+
+  // '\'' Agregar comilla simple //
+   var cadena3= "Hola \' mundo";
+   system.out.println("cadena3 = "+ cadena3);
+  
+  // '\"' agregar una comilla doble //
+   var cadena4= "Hola \" mundo";
+   system.out.println("cadena4 = "+ cadena4);
+
+  // '\\' Caracter diagonal invertida //
+   var cadena5= "Hola \\  mundo";
+   system.out.println("cadena5 = "+ cadena5);
+}
